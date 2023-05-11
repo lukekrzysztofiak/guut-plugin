265f67bbc860aad1cced97e5e40ccaad4424fd57
lukekrzysztofiak <87997353+lukekrzysztofiak@users.noreply.github.com>
2023-05-11 09:46:30 -0500

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -3,13 +3,13 @@
  * Plugin Name: FontStyles Plugin
  */
 
-function my_custom_block_enqueue() {
+function font_styles_block_enqueue() {
     wp_enqueue_script(
-        'my-custom-block',
+        'font_styles',
         plugins_url( 'build/index.js', __FILE__ ),
         array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ),
         filemtime( plugin_dir_path( __FILE__ ) . 'build/index.js' ),
         true
     );
 }
-add_action( 'enqueue_block_editor_assets', 'my_custom_block_enqueue' );
+add_action( 'enqueue_block_editor_assets', 'font_styles_block_enqueue' );
